6ec37ef0713344002f5ac49e87b58a14ad437a88
João Trepichio <trepichio@gmail.com>
2021-08-29 03:12:24 -0300

    Import Mailer Module configuration Add Async config to load generated auth credentials of Ethereal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,45 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProxyrmqModule } from './proxyrmq/proxyrmq.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import * as nodemailer from 'nodemailer';
+const configService = new ConfigService();
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
     ProxyrmqModule,
+    MailerModule.forRootAsync({
+      useFactory: async () => {
+        /**
+         * Generate Authentication for SMTP (for Testing purposes only)
+         * For production use, use your own credentials
+         */
+        let testAccount = await nodemailer.createTestAccount();
+
+        /**
+         * Setup SMTP transport
+         */
+        return {
+          transport: {
+            host: configService.get<string>('MAIL_HOST'),
+            port: configService.get<number>('MAIL_PORT'),
+            secure: false,
+            auth: {
+              /* user: configService.get<string>('MAIL_USER'), */
+              /* pass: configService.get<string>('MAIL_PASS'), */
+              user: testAccount.user, // generated ethereal user
+              pass: testAccount.pass, // generated ethereal password
+            },
+          },
+          defaults: {
+            from: configService.get<string>('MAIL_FROM'),
+          },
+        };
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
