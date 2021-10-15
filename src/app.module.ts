3bb775196c3715b23bfaa33c278713e7f28a9345
João Trepichio <trepichio@gmail.com>
2021-10-15 10:39:16 -0300

    Fix dynamic Mailer config for production and dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,19 +7,37 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import * as nodemailer from 'nodemailer';
 import { ClientProxySmartRanking } from './proxyrmq/client-proxy.provider';
 
-const configService = new ConfigService();
+const logger = new Logger('AppModule');
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
     ProxyrmqModule,
     MailerModule.forRootAsync({
-      useFactory: async () => {
-        /**
-         * Generate Authentication for SMTP (for Testing purposes only)
-         * For production use, use your own credentials
-         */
-        let testAccount = await nodemailer.createTestAccount();
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const account = {} as any;
+        const isDevMode: boolean =
+          configService.get<string>('NODE_ENV') === 'development';
+
+        logger.log(`isDevMode: ${isDevMode}`);
+
+        if (isDevMode) {
+          /**
+           * Generate Authentication for SMTP (for Testing purposes only)
+           * For production use, use your own credentials
+           */
+          const testAccount = await nodemailer.createTestAccount();
+          account.user = testAccount.user; // generated ethereal user
+          account.pass = testAccount.pass; // generated ethereal password
+        } else {
+          /**
+           * For production use, use your own credentials
+           */
+          account.user = configService.get<string>('MAIL_USER');
+          account.pass = configService.get<string>('MAIL_PASS');
+        }
 
         /**
          * Setup SMTP transport
@@ -30,10 +48,8 @@ const configService = new ConfigService();
             port: configService.get<number>('MAIL_PORT'),
             secure: false,
             auth: {
-              /* user: configService.get<string>('MAIL_USER'), */
-              /* pass: configService.get<string>('MAIL_PASS'), */
-              user: testAccount.user, // generated ethereal user
-              pass: testAccount.pass, // generated ethereal password
+              user: account.user,
+              pass: account.pass,
             },
           },
           defaults: {
